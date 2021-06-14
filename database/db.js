b0fcd288dcc8de782615f21519ae7a479784b80d
ReeN262 <korobk.vitaly@gmail.com>
2021-06-15 01:12:45 +0300

    db

--- database/db.js
+++ database/db.js
@@ -1,14 +1,14 @@
 const knex = require('knex')({
     client: 'postgres',
     connection: {
-      host: 'localhost',
-      user: 'postgres',
-      password: 'maxkok97',
-      database: 'db'
-      // host: 'ec2-54-90-211-192.compute-1.amazonaws.com',
-      // user: 'qkyyfwjwpncacq',
-      // password: '0e555a8c3a16fd501419527ec03cb68614eb6fb7182fa9b945be97d4e14a06c7',
-      // database: 'd9g5g1n9l35ea9'
+      // host: 'localhost',
+      // user: 'postgres',
+      // password: 'maxkok97',
+      // database: 'db'
+      host: 'ec2-54-90-211-192.compute-1.amazonaws.com',
+      user: 'qkyyfwjwpncacq',
+      password: '0e555a8c3a16fd501419527ec03cb68614eb6fb7182fa9b945be97d4e14a06c7',
+      database: 'd9g5g1n9l35ea9'
     },
   },
 );
