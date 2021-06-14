07b331d03c506c9501b0e0527e18ec62ffe42d3a
ReeN262 <korobk.vitaly@gmail.com>
2021-06-15 02:17:52 +0300

    db

--- database/db.js
+++ database/db.js
@@ -9,7 +9,8 @@ const knex = require('knex')({
       host: 'ec2-54-90-211-192.compute-1.amazonaws.com',
       user: 'qkyyfwjwpncacq',
       password: '0e555a8c3a16fd501419527ec03cb68614eb6fb7182fa9b945be97d4e14a06c7',
-      database: 'd9g5g1n9l35ea9'
+      database: 'd9g5g1n9l35ea9',
+      ssl: true
     },
   },
 );
